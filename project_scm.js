048ae92df35820b20fa8252d7b2f61775e7ac792
AkankshaRana1903 <157495063+AkankshaRana1903@users.noreply.github.com>
2024-03-29 11:36:08 +0530

    Update project_scm.js

--- project_scm.js
+++ project_scm.js
@@ -1,6 +1,5 @@
 const hamburger = document.getElementById('hamburger'); 
 const menu = document.querySelector('.menu'); 
-
 hamburger.addEventListener('click', function () { 
 	const hamIcon = this.querySelector('.hamburger-icon'); 
 	const crossIcon = this.querySelector('.cross-icon'); 
